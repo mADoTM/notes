adff02feeab59621f06518342ed629ae673c3fd7
Максим Долженко <maxim_2003@mail.ru>
2023-03-05 07:55:36 +0300

    Добавил логер

--- src/main/java/ru/severstal/notes/service/NoteService.java
+++ src/main/java/ru/severstal/notes/service/NoteService.java
@@ -1,6 +1,7 @@
 package ru.severstal.notes.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.severstal.notes.entity.Note;
 import ru.severstal.notes.repository.NoteRepository;
@@ -9,28 +10,34 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NoteService {
     private final NoteRepository noteRepository;
 
     public List<Note> all() {
+        log.info("RETURNING ALL NOTES FROM DB");
         return (List<Note>) noteRepository.findAll();
     }
 
     public Note getById(long id) {
+        log.info("SEARCHING NOTE IN DB WITH ID {}", id);
         return noteRepository
                 .findById(id)
                 .orElseGet(Note::new);
     }
 
     public void saveNote(Note note) {
+        log.info("SAVED NOTE - {}", note);
         noteRepository.save(note);
     }
 
     public void deleteNote(Note note) {
+        log.info("DELETING NOTE - {}", note);
         noteRepository.delete(note);
     }
 
     public void deleteById(long id) {
+        log.info("DELETING NOTE BY ID - {}", id);
         noteRepository.deleteById(id);
     }
 }
